71541f65c0b1b4b8a424eeeec74697949dcd1b97
Aleš Kůtek <kutekales@gmail.com>
2021-04-06 15:25:19 +0200

    performance

--- app/Performance.js
+++ app/Performance.js
@@ -2,9 +2,15 @@
 import React, { PureComponent } from 'react';
 import { Button, FlatList, Image, Modal, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+const image = require("./image.png");
+h
 export default class Performance extends PureComponent {
   state = {
     items: {},
+  }
+
+  componentWillMount() {
+    this.start = new Date();
   }
 
   componentDidMount() {
@@ -13,7 +19,7 @@ export default class Performance extends PureComponent {
     for (var i = 0; i < 10000; i++) {
       list.push({
         text: "Text .... " + i.toString(),
-        key: i,
+        key: i.toString(),
       });
     }
 
@@ -22,13 +28,20 @@ export default class Performance extends PureComponent {
     });
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-    this.interval = null;
+  removeEl(key) {
+    var items = [...this.state.items];
+
+    items.splice(key, 1);
+
+    this.setState({
+      items: items,
+    })
   }
 
   render() {
     const {items} = this.state;
+
+    console.log(new Date().getTime() - this.start.getTime());
 
     return (
       <>
@@ -36,9 +49,11 @@ export default class Performance extends PureComponent {
           data={items}
           renderItem={(item) => (
             <View style={{flexDirection: "row"}}>
-              <Image style={{flex: 1}} source={require("./image.png")} />
+              <Image style={{flex: 1}} source={image} />
               <Text style={{flex: 1}}>{item.item.text}</Text>
-              <Button style={{flex: 1}} title="Tap me"/>
+              <Button style={{flex: 1}} title="Tap me" onPress={ev => {
+                this.removeEl(item.item.key)
+              }}/>
             </View>
           )}
         />
